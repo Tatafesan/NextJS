83db9709bb993cdb987f01b1a42dc4fea17cfbcd
Tatafesan <talitafesan@gmail.com>
2023-02-13 17:00:48 -0400

    Assets, metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,9 +3,10 @@ import Link from 'next/link';
 export default function FirstPost() {
     return (
         <>
+        <img src="/images/profile.jpg" alt="Your Name" />
         <h1>First Post</h1>
         <h2>
-            <Link href="/"> Back to home</Link>
+            <Link href="/">Voltar Home</Link>
         </h2>
         </>
     )
